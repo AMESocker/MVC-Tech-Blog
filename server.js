2ed1f5019f85d43f27cab9bf655975d06ff24dfd
AMESocker <aesocher@yahoo.com>
2022-11-06 17:59:37 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-//Criteria
-//Homepage-existing blog posts
-//Homepage-navigation links
-//Homepage-option to log in
-//Homepage link links to Homepage
-//other links promoted to sign-in
-//sign-up button-user credentials are saved and logged into the site
-//revisit the site at a later time and choose to sign in user prompted to enter username and password
-//after signed in-links for the homepage, the dashboard, and the option to log out are visible
-//existing blog posts that include the post title and the date created
-//click on an existing blog post-presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
-// enter a comment and click on the submit button while signed in THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
-// click on the dashboard option in the navigation THEN I am taken to the dashboard and presented with any blog posts I have already created and the option to add a new blog post
-//new blog button area to enter title and contents
-//new blog button-title and contents saved then back to dashboard
-//dashboard post update or delete then back to dashboard
-//navigation-logout
-//idle time sign out
-
-const expHbars = require('express-handlebars');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// Dependencies
+//Express
 const express = require('express');
+//Express-handlebars
+const exphbs = require('express-handlebars');
+//ExpressHandlebars instances
+const path = require('path');
+//Node.js module Create a session middleware
+const hbs = exphbs.create({});
+
+//node.js-provides utilities for working with file and directory paths
+const session = require('express-session');
+//a SQL session store using Sequelize.js
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
+//Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Sets up Handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Sets up the routes
+app.use(require('./controllers/routes'));
+
+//Starts the server to begin listening
+app.listen(PORT, () => {
+    console.log('Server listening on: http://localhost:' + PORT);
+  });
