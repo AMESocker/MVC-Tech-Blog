2c628422aacb5dc0871d3e5260ad40f94e5f3b17
AMESocker <aesocher@yahoo.com>
2022-11-07 08:28:43 -0800

    added file structure and Psudeo code

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,8 @@
+
+
+
+//Create New User
+
+//Login
+
+//Logout
---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+//----routes----
+//homepage --> homepage
+//other links --> sign up/sign in
+//sign up --> prompt to create username & password
+//sign up button --> info saved then logged in
+//dashboard 
+//login
+//log out --> signed out
+//idle --> signed out
+//existing blog post
+//comment submit
+//new blog post
+//create blog post
+//existing blog post
+//update post --> dashboard
+//delete post --> dashboard
+
+
+
